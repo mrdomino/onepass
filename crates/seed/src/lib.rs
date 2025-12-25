4af48c1caad114be3bd5432c1bdd655a51840a3f
Steven Dee <git@wholezero.org>
2025-12-24 19:30:25 -0800

    refactor: move tsv macros from base to seed

--- crates/seed/src/lib.rs
+++ crates/seed/src/lib.rs
@@ -1,14 +1,63 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+/// Write the fields passed to this macro as tab-separated values with all inner TSV-meaningful
+/// characters escaped; see [`format_tsv_args`].
+#[macro_export]
+macro_rules! write_tsv {
+    ($w:expr, $($args:tt)+) => {
+        $w.write_fmt($crate::format_tsv_args!($($args)+))
+    };
+}
+
+/// Format the fields passed to this macro as tab-separated values with all inner TSV-meaningful
+/// characters escaped; see [`format_tsv_args`].
+#[macro_export]
+macro_rules! format_tsv {
+    ($($args:tt)+) => {
+        std::fmt::format($crate::format_tsv_args!($($args)+))
+    };
+}
+
+/// Format the fields passed to this macro as a [`core::fmt::Arguments`] that formats the fields
+/// with TSV characters escaped.
+#[macro_export]
+macro_rules! format_tsv_args {
+    ($first:expr $(, $($rest:tt)+)?) => {
+        $crate::format_tsv_args!(
+            @build
+            "{}",
+            (onepass_base::fmt::TsvField($first))
+            $(, $($rest)+)?
+        )
+    };
+    (@build $fmt:expr, ($($args:tt)+)) => {
+        core::format_args!($fmt, $($args)+)
+    };
+    (@build $fmt:expr, ($($args:tt)+), $next:expr $(, $($rest:tt)+)?) => {
+        $crate::format_tsv_args!(
+            @build
+            concat!($fmt, "\t{}"),
+            ($($args)+, onepass_base::fmt::TsvField($next))
+            $(, $($rest)+)?
+        )
+    };
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use std::io::{BufWriter, Write};
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn format_tsv_works() {
+        assert_eq!("", &format_tsv!(""));
+        assert_eq!("\t", &format_tsv!("", ""));
+        assert_eq!("a\tb\tc", &format_tsv!("a", "b", "c"));
+        assert_eq!("a\\r\\\\\\t\\n\tb", &format_tsv!("a\r\\\t\n", "b"));
+    }
+
+    #[test]
+    fn write_tsv_works() {
+        let mut buf = BufWriter::new(Vec::new());
+        write_tsv!(buf, "a\nb", "c\td").unwrap();
+        let s = String::from_utf8(buf.into_inner().unwrap()).unwrap();
+        assert_eq!("a\\nb\tc\\td", &s);
     }
 }
