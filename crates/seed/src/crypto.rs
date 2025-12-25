46d61b48673718c2f9c8ecc2ca3415cc06338f77
Steven Dee <git@wholezero.org>
2025-12-24 22:02:34 -0800

    feat(seed): add new `secret_uniform`
    This implements the incoming `crypto-bigint` 0.7.0 random modulus scheme
    in `onepass-seed`. It is a separate function to decouple it from the
    slow Argon2 derivation in testing and in case we ever want to generate
    passwords from other sources, e.g. secure OS randomness.
    
    This commit also makes `Derivation` public.

--- crates/seed/src/crypto.rs
+++ crates/seed/src/crypto.rs
@@ -1,8 +1,12 @@
 use core::fmt::{Display, Write};
+use std::cmp::Ordering;
 
 use argon2::{Algorithm, Argon2, Params, Version};
 use blake2::{Blake2b256, Digest};
+use chacha20::ChaCha20Rng;
+use crypto_bigint::{Encoding, NonZero, U256};
 use onepass_base::fmt::DigestWriter;
+use rand_core::{RngCore, SeedableRng};
 use zeroize::Zeroizing;
 
 use crate::{data::Site, write_tsv};
@@ -26,7 +30,25 @@ impl Site {
     }
 }
 
-struct Derivation<'a>(&'a Site);
+pub fn secret_uniform(secret: &[u8; 32], n: &NonZero<U256>) -> Zeroizing<U256> {
+    let mut rng = ChaCha20Rng::from_seed(*secret);
+    let mut ret = U256::ZERO.to_le_bytes();
+    let n_bits = n.bits_vartime();
+    let n_bytes = n_bits.div_ceil(8) as usize;
+    let hi_mask = !0 >> ((8 - (n_bits % 8)) % 8);
+
+    loop {
+        rng.fill_bytes(&mut ret[..n_bytes]);
+        ret[n_bytes - 1] &= hi_mask;
+        let ret = Zeroizing::new(U256::from_le_bytes(ret));
+        match ret.cmp(n) {
+            Ordering::Less => return ret,
+            _ => (),
+        }
+    }
+}
+
+pub struct Derivation<'a>(&'a Site);
 
 impl Display for Derivation<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -85,5 +107,58 @@ mod tests {
             "cd319a18cd2e86ef74805e91ce0b74b52db8b9b6252bc6dbb38cd9c3fdc07191",
             hex::encode(test_site().secret("testpass2"))
         );
+        let mut site2 = test_site();
+        site2.increment = 1;
+        site2.username = Some("you@example.com".into());
+        assert_eq!(
+            "dc4354071b6b73bc8021f2b9d190298155fe79e8eff746a7290299110899c8e4",
+            hex::encode(site2.secret("testpass"))
+        );
+    }
+
+    #[test]
+    fn secret_uniform_vectors() {
+        let tests: [(&str, &str, &str); _] = [
+            (
+                "0000000000000000000000000000000000000000000000000000000000000000",
+                "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+                "C70D778BCCEF36A81AED8DA0B819D2BD28BD8653E56A5D40903DF1A0ADE0B876",
+            ),
+            (
+                "0123456789abcdeffedcba98765432100123456789abcdeffedcba9876543210",
+                "0000000000000000000000000000000000000000000000000000000000100000",
+                "000000000000000000000000000000000000000000000000000000000005D415",
+            ),
+            (
+                "0123456789abcdeffedcba98765432100123456789abcdeffedcba9876543210",
+                "295A7969D28101E13473A8DD15E68D28CCD4F578591D8994008C5D999F85D416",
+                "295A7969D28101E13473A8DD15E68D28CCD4F578591D8994008C5D999F85D415",
+            ),
+            (
+                "0123456789abcdeffedcba98765432100123456789abcdeffedcba9876543210",
+                "295A7969D28101E13473A8DD15E68D28CCD4F578591D8994008C5D999F85D415",
+                "0D313C0A2DDB1AE37A6EF3ECC18F8588FB946C5BE4A31B39784D7C9530E31D51",
+            ),
+            (
+                "0000000000000000000000000000000000000000000000000000000000000000",
+                "0000000000000000000000000000000000000000000000000000000000000001",
+                "0000000000000000000000000000000000000000000000000000000000000000",
+            ),
+            (
+                "a96d610f969d8befcc5a8f7db635976eeb5c83718a2a0d9974a4bb1b6423fac9",
+                "00000000000000001FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+                "00000000000000000B6CE7C37CBAA4C1133D97B36751CCE9AA56B264F9E8698D",
+            ),
+        ];
+        for (sec, siz, want) in tests {
+            let sec = U256::from_be_hex(sec);
+            assert_eq!(
+                U256::from_be_hex(want),
+                *secret_uniform(
+                    &sec.to_be_bytes(),
+                    &NonZero::new(U256::from_be_hex(siz)).unwrap()
+                ),
+            );
+        }
     }
 }
