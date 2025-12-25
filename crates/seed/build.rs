7a289f930f353ad0437d5d0204ca6d5b98be41fc
Steven Dee <git@wholezero.org>
2025-12-25 10:31:11 -0800

    style(seed): code golf in build.rs, const hash
    32 bytes seems small enough to be `const`. Shorter code is better code.
    This code could also be clearer and longer, but before it was both long
    and unclear, so this is an improvement.

--- crates/seed/build.rs
+++ crates/seed/build.rs
@@ -9,45 +9,36 @@ use onepass_base::dict::{BoxDict, Dict};
 
 fn main() {
     println!("cargo:rerun-if-changed=data/eff_large_wordlist.txt");
-    let out_dir = env::var_os("OUT_DIR").unwrap();
-    let mut dest_path = PathBuf::from(&out_dir);
-    dest_path.push("wordlist.rs");
     let f = File::open("data/eff_large_wordlist.txt").unwrap();
     let reader = BufReader::new(f);
     let words = reader
         .lines()
-        .try_fold(Vec::new(), |mut words, line| {
-            let line = line?;
-            if line.is_empty() {
-                return Err(Error::other("empty line"));
-            }
-            let word = line
-                .split('\t')
-                .nth(1)
-                .ok_or_else(|| Error::other("malformed word list"))?;
-            words.push(word.to_string());
-            Ok(words)
-        })
+        .map(|line| -> Result<Option<_>, Error> { Ok(line?.split('\t').nth(1).map(Box::from)) })
+        .filter_map(Result::transpose)
+        .collect::<Result<Box<[_]>, _>>()
         .unwrap();
     let dict = BoxDict::from_iter(words.iter().map(AsRef::as_ref));
+
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let mut dest_path = PathBuf::from(&out_dir);
+    dest_path.push("wordlist.rs");
     let mut f = File::create(dest_path).unwrap();
     writeln!(
         f,
         "// Generated at build time from data/eff_large_wordlist.txt"
     )
     .unwrap();
-    writeln!(f, "static EFF_WORDLIST_HASH: [u8; 32] = [").unwrap();
-    for &b in dict.hash() {
-        writeln!(f, "    0x{:02x},", b).unwrap();
+
+    let hash = dict.hash();
+    writeln!(f, "const EFF_WORDLIST_HASH: [u8; {}] = [", hash.len()).unwrap();
+    for &b in hash {
+        writeln!(f, "    0x{b:02x},").unwrap();
     }
     writeln!(f, "];").unwrap();
-    writeln!(
-        f,
-        "static EFF_WORDLIST_WORDS: [&str; {}] = [",
-        dict.words().len()
-    )
-    .unwrap();
-    for &word in dict.words() {
+
+    let words = dict.words();
+    writeln!(f, "static EFF_WORDLIST_WORDS: [&str; {}] = [", words.len(),).unwrap();
+    for &word in words {
         writeln!(f, "    {word:?},").unwrap();
     }
     writeln!(f, "];").unwrap();
