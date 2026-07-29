9c25465438c9f708f765d5a6ade53e77e7663cb7
Steven Dee <git@wholezero.org>
2026-07-29 01:19:31 -0700

    chore(deps): switch to new keyring-rs ecosystem
    Migrates to the keyring v4 / keyring-core v1 API, removing a lot of
    ancient deps and also dropping some home-baked code.

--- src/seed_password/keyring/mod.rs
+++ src/seed_password/keyring/mod.rs
@@ -1,26 +1,62 @@
-#[cfg(not(keyring = "rs"))]
-mod error;
-#[cfg(keyring = "macos")]
-mod macos_keychain;
 #[cfg(keyring = "no")]
 mod null_keyring;
 
-use anyhow::{Context, Result};
+use std::sync::OnceLock;
 
-#[cfg(keyring = "rs")]
-pub(super) use keyring::{Entry, Error};
-
-#[cfg(keyring = "macos")]
-pub(super) use macos_keychain::Entry;
-#[cfg(keyring = "no")]
-pub(super) use null_keyring::Entry;
-
-#[cfg(not(keyring = "rs"))]
-pub(super) use error::Error;
+use anyhow::{self, Context};
+use keyring_core::{self, Entry, set_default_store};
 
 const SERVICE: &str = "onepass.app.whilezero.org";
 const ACCOUNT: &str = "seed";
 
-pub(super) fn get_entry() -> Result<Entry> {
+fn setup_store() -> keyring_core::Result<()> {
+    #[cfg(keyring = "no")]
+    {
+        use null_keyring::Store;
+        set_default_store(Store::new()?);
+        Ok(())
+    }
+    #[cfg(keyring = "macos")]
+    {
+        use apple_native_keyring_store::protected::Store;
+        set_default_store(Store::new()?);
+        Ok(())
+    }
+    #[cfg(keyring = "rs")]
+    {
+        #[cfg(target_os = "linux")]
+        {
+            use zbus_secret_service_keyring_store::Store;
+            set_default_store(Store::new()?);
+            Ok(())
+        }
+        #[cfg(target_os = "macos")]
+        {
+            use apple_native_keyring_store::keychain::Store;
+            set_default_store(Store::new()?);
+            Ok(())
+        }
+        #[cfg(target_os = "windows")]
+        {
+            use windows_native_keyring_store::Store;
+            set_default_store(Store::new()?);
+            Ok(())
+        }
+        #[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows")))]
+        {
+            Err(Error::NotSupportedByStore(
+                "Unsupported platform".to_string(),
+            ))
+        }
+    }
+}
+
+static START: OnceLock<keyring_core::Result<()>> = OnceLock::new();
+
+pub(super) fn get_entry() -> anyhow::Result<Entry> {
+    match START.get_or_init(setup_store) {
+        Ok(()) => (),
+        Err(err) => anyhow::bail!("Store setup failed: {err}"),
+    }
     Entry::new(SERVICE, ACCOUNT).context("failed getting keyring entry")
 }
